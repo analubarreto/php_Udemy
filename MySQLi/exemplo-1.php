6cfe012272f70f2705029b2aedc5818d00bdebac
analubarreto <analuibm@gmail.com>
2019-02-03 03:00:25 -0200

    php e mysql

--- MySQLi/exemplo-1.php
+++ MySQLi/exemplo-1.php
@@ -1,7 +1,17 @@
 <?php
 
-$conn = new mysqli("localhost", "root", " ", dbphp7);
+// acessar banco de dados
+$conn = new mysqli("localhost", "root", "15926535", dbphp7);
 
 if ($conn->connect_error) {
     echo "Error: ". $conn->connect_error;
 }
+
+// preparar um comando para enviar para o banco de dados
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (des_login, des_senha) VALUES(?, ?)");
+
+// primeiro parâmetro, tipos das interrogações (s de string, sem vírgula)
+// segundo parâmetro valores
+$stmt->bind_param("ss", "user", "12345");
+
+$stmt->execute();
